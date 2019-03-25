65f03716c465b58a2ab29a730ae943331fd3f2da
LipricoN <lipricon74@gmail.com>
2019-03-25 22:23:44 +0500

    Выполнено задание 5-1

--- Lesson_5/1.py
+++ Lesson_5/1.py
@@ -5,3 +5,52 @@
 вывести наименования предприятий, чья прибыль выше среднего и отдельно
 вывести наименования предприятий, чья прибыль ниже среднего.
 """
+
+from collections import defaultdict
+
+num_com = int(input('Введите количество предприятий: '))
+
+company = defaultdict(list)
+
+for n in range(num_com):
+    name = input(f'Введите название организации {n+1}: ')
+    all_val = input('Введите прибыль за четыре квартала разделенную пробелами: ').split()
+    all_val = list(map(float, all_val))
+    
+    tmp_val = 0
+    for n in all_val:
+        tmp_val += n
+        company[name].append(n)
+    company[name].append(tmp_val)
+
+avg_val = 0
+
+for key in company.keys():
+    avg_val += company[key][4]
+
+avg_val = avg_val / len(company)
+
+print()
+print(f'{"Имя/Квартал":^12}{"1":^8}{"2":^8}{"3":^8}{"4":^8}{"Год":^8}')
+
+for key in company.keys():
+    print(f'{key:<12}', end='')
+    for val in company[key]:
+        print(f'{val:^8}', end='')
+    print()
+
+print()
+print(f'Средняя прибыль предприятий за год: {avg_val}')
+
+min_avg = []
+max_avg = []
+
+for key in company.keys():
+    if company[key][4] > avg_val:
+        max_avg.append(key)
+    
+    if company[key][4] < avg_val:
+        min_avg.append(key)    
+
+print(f'Предприятия, чья прибыль выше среднего: {", ".join(max_avg)}')
+print(f'Предприятия, чья прибыль ниже среднего: {", ".join(min_avg)}')
